aea2277915b7d4f4f13150955edf83e020ebbaf6
wglee0511 <ranazang@naver.com>
2020-12-02 21:35:19 +0900

    Chat socket in console

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
 
 app.set('views','src/views'); 
 app.set("view engine", "pug");
+app.use(logger("dev"));
 
 app.use(express.static("src/static"));
 // it is same with app.use(express.static(join(_dirname, "static")))
@@ -18,6 +20,18 @@ const hadleServerListener = () => {
     console.log(` ❤ Server is rumming : https://localhost:${PORT} `)
 }
 
-app.listen(PORT,hadleServerListener);
+const server =  app.listen(PORT,hadleServerListener);
+const io = socketIO(server);
 
+io.on("connection", socket => {
+    socket.on("newMessage", ({message}) => {
+        socket.broadcast.emit("messageNoti",{ 
+            message,
+            nickname : socket.nickname || "Anon"
+         })
+    });
+    socket.on("setNickName", ({nickname}) => {
+        socket.nickname = nickname;
+    })
+});
 
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,19 @@
 // static is for front-end
+
+const socket = io("/");
+
+function sendMessage(message){
+    socket.emit("newMessage", { message : message })
+    console.log(`You : ${message}`)
+}
+
+function setNickName(nickname){
+    socket.emit("setNickName", { nickname : nickname })
+}
+
+function handleNoti(data){
+    const {message, nickname} = data;
+    console.log( `${nickname} : ${message}` )
+}
+
+socket.on("messageNoti",handleNoti)
